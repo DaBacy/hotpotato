fc35fc747942bc9b362ddcac280f49b3e2ae1240
DaBacy <102991629+DaBacy@users.noreply.github.com>
2022-04-13 12:39:46 -0600

    cool stuff

--- main.ts
+++ main.ts
@@ -1,28 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # # # .
-        . . . # .
-        . # # # .
-        . . . # .
-        . # # # .
-        `)
-    basic.clearScreen()
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . . . # .
-        . . # . .
-        . # # # #
-        `)
-    basic.clearScreen()
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
-    basic.clearScreen()
+    strip.clear()
+    range3.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    strip.clear()
+    range2.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(1000)
+    strip.clear()
+    range.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(1000)
+    strip.clear()
     random_number = randint(5, 15)
     while (random_number > 0) {
         basic.clearScreen()
@@ -31,6 +17,13 @@ input.onButtonPressed(Button.A, function () {
         basic.showIcon(IconNames.Square)
     }
     if (random_number == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
         basic.showIcon(IconNames.SmallDiamond)
         basic.showIcon(IconNames.Diamond)
         basic.showLeds(`
@@ -43,7 +36,15 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 let random_number = 0
-basic.showString("Hi")
+let range: neopixel.Strip = null
+let range2: neopixel.Strip = null
+let range3: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
+range3 = strip.range(11, 14)
+range2 = strip.range(6, 11)
+range = strip.range(0, 6)
+strip.showColor(neopixel.rgb(randint(0, 64), randint(0, 64), randint(0, 64)))
 basic.forever(function () {
 	
 })
